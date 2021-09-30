248e0bd1588ed2e70fefbaff867d0755a7d6eef9
sdavitt <sdavt2@gmail.com>
2021-09-30 12:09:03 -0500

    fixed .env typo

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,14 +1,14 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, json, jsonify
 from app.models import Actor, db
 
 api = Blueprint('api', __name__, url_prefix='/api')
+
 
 @api.route('/actors', methods=['GET'])
 def actors():
     """
     [GET] /api/actors
-    Provides a list of all actors in our database.
+    Returns JSON data about all actors in our database
     """
-    # query our database to get all of our current actor data and transform to a dictionary with a dictionary comprehension
     actors = {(a.first_name+' '+a.last_name).strip():a.to_dict() for a in Actor.query.all()}
     return jsonify(actors)
--- config.py
+++ config.py
@@ -12,5 +12,5 @@ class Config:
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABSE_URI') # reminder: add 'ql' to the end of 'postgres' to make 'postgresql' in the database_uri in the .env file
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') # reminder: add 'ql' to the end of 'postgres' to make 'postgresql' in the database_uri in the .env file
     SQLALCHEMY_TRACK_MODIFICATIONS = False
